0542ee5ef6305df597359fb2a4152f78c3574290
Juliana Witzke de Brito <jbrito@ef5.local>
2020-03-09 17:23:09 -0300

    Desenvolvimento do jogo

--- script.js
+++ script.js
@@ -2,7 +2,11 @@
 sequencia = lista de cores 
 pessoa tenta acertar a sequencia em 10s e se acertar clearInterval, proximo nivel (proximo elemento lista)
 se errar, alert
-*/ 
+
+
+btY.classList.add("piscar");
+
+*/
 let botao = document.querySelector("#botao");
 let btG = document.querySelector(".green");
 let btY = document.querySelector(".yellow");
@@ -19,31 +23,111 @@ let niveis = [{btY},             //NIVEL 1
 */
 
 let niveis = [
-    [btY],
-    [btY, btG],
-    [btY, btG, btB],
-    [btY, btG, btB, btR]
+    [], [], [], [], []
+]
+
+let resposta = [
+    [], [], [], [], []
 ]
 
 let indice;
-let nivelAtual;
+let i, j = 0;
+let nivelAtual = 0;
 function comecar(){
-    nivelAtual = 1;
-    for(indice=0; indice<niveis.length; indice++){
-        executarNivel(niveis[i], i+1);
-        armazenarResposta();
-    } 
+    nivelAtual += 1;
+    executarNivel(nivelAtual);
 }
 
-let i=0;
-function executarNivel(nivel, nNivel){
-    for (i=0; i<nNivel; i++){
-        nivel[i].classList.add("piscar");
+function randomNumbers (numeroDeNiveis){
+    let numerosSorteados = [];
+    for (i=0; i<numeroDeNiveis; i++) {
+        //Seleciona numero de 1 a 4
+        numerosSorteados[i] = Math.floor(Math.random()*(4-1+1)+1); //Math.random()*(max-min+1)+min
+    }
+    return numerosSorteados;
+}
+
+function piscaCor(numeroCor, i, nivelAtual) {
+    switch (numeroCor) {
+        case 1:
+            btG.classList.add("piscar");
+            setTimeout(function(){ btG.classList.remove("piscar"); }, 500);
+            niveis[nivelAtual-1][i] = 1;
+            break;
+        case 2:
+            btR.classList.add("piscar");
+            setTimeout(function(){ btR.classList.remove("piscar"); }, 500);
+            niveis[nivelAtual-1][i] = 2;
+            break;
+        case 3:
+            btB.classList.add("piscar");
+            setTimeout(function(){ btB.classList.remove("piscar"); }, 500);
+            niveis[nivelAtual-1][i] = 3;
+            break;
+        case 4:
+            btY.classList.add("piscar");
+            setTimeout(function(){ btY.classList.remove("piscar"); }, 500);
+            niveis[nivelAtual-1][i] = 4;
+            break;
+        default:
+            break;
     }
 }
 
-function armazenarResposta(){
+function falhou() {
+    console.log("Falhou");
 }
+
+function armazenaResposta(corCerta, i, NivelAtual) {
+
+    btG.onclick = function () {
+        if (corCerta == 1) {
+            console.log("Acertou");
+            resposta[nivelAtual-1][i] = 1;
+            comecar();
+        } else { console.log ("Errou"); }
+    }
+    btR.onclick = function () {
+        if (corCerta == 2) {
+            console.log("Acertou");
+            resposta[nivelAtual-1][i] = 2;
+            comecar();
+        } else { console.log ("Errou"); }
+    }
+    btB.onclick = function () {
+        if (corCerta == 3) {
+            console.log("Acertou");
+            resposta[nivelAtual-1][i] = 3;
+            comecar();
+        } else { console.log ("Errou"); }
+    }
+    btY.onclick = function () {
+        if (corCerta == 4) {
+            console.log("Acertou");
+            resposta[nivelAtual-1][i] = 4;
+            comecar();
+        } else { console.log ("Errou"); }
+    }
+}
+
+function executarNivel(nivelAtual){
+    let numerosSorteados = randomNumbers(nivelAtual);
+    for (i= 0; i<numerosSorteados.length; i++){
+        let cor = numerosSorteados[i];
+        piscaCor(cor, i, nivelAtual);
+        armazenaResposta(cor, i, nivelAtual);
+        // setInterval(function(){ falhou(); }, 5000);
+        console.log("FINAL---------------")
+        console.log("Cor:"+cor);
+        console.log("i:"+i);
+        console.log("Nivel Atual:"+nivelAtual);
+    }
+    console.log("Numeros sorteados:")
+    console.log(numerosSorteados);
+    console.log(niveis);
+    console.log(resposta);
+}
+
 
 //piscar = aparecer e sumir o brilho
 botao.onclick = comecar;
